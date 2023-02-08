fea6e36b05c9454ebb6d490dc510e15a662f96c9
MGonza20 <sara.paguaga@gmail.com>
2023-02-08 00:18:50 -0600

    Añadiendo padres

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ class Graph:
         if len(self.graph) > 0:
             # En caso de que no dxista el nodo se crea ese nodo 
             nObj = [obj for obj in self.graph if obj.value == node]
-            # En caso de que no dxista el nodo se crea ese nodo 
+            # En caso de que no exista el nodo se crea ese nodo 
             if not any(node == obj.value for obj in self.graph): 
                 nodee = nodo(node, [edge], [], None)
                 self.graph.append(nodee) 
+                for nodeG in self.graph:
+                    if nodee.value in nodeG.dependencies:
+                        nodee.parent.append(nodeG.value)
             # Si existe la arista que se quiere agregar en 
             # la lista de dependencias de ese nodo tira error
             elif edge in nObj[0].dependencies:
-                raise ValueError("Arista repetida.")
+                raise ValueError("No se puede añadir una arista repetida.")
             # Sino hace append 
             else:
                 nObj[0].dependencies.append(edge)          
@@ -39,17 +42,17 @@ class Graph:
         elif self.notInRange(prob):
              raise ValueError("Las números deben encontrarse en un rango de 0 y 1")
         else:
-            nObj[0].probabilities = prob
+            nObj[0].probabilities.append(prob)
 
 
 graph = Graph()
 graph.addEdge(2, 1)
 graph.addEdge(1, 3)
 graph.addEdge(2, 3)
-graph.addEdge(1, 3)
+# graph.addEdge(1, 3)
 
 for j in graph.graph:
-    print(j.value, j.dependencies)
+    print(j.value, j.dependencies, j.parent)
 
  
 #     def checkConections(self):
