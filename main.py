d3f33e91fc407215429256859838afacd19dccf7
MGonza20 <sara.paguaga@gmail.com>
2023-02-09 11:03:07 -0600

    Regresando factores

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 from nodo import nodo
+import itertools
 
 class Graph:
     def __init__(self):
@@ -69,22 +70,78 @@ class Graph:
                 concat = ",".join(node.parent)
                 jointList.append(f"P({node.value}|{concat})")
         return("⋅".join(jointList))
+
+    def genKeys(self, node):
+        sim_s = []
+        nodee = [obj for obj in self.graph if obj.value == node][0]
+        
+        if nodee.parent:
+            sim_s += nodee.parent
+        sim_s.append(nodee.value)
         
 
+        # Generando posibles combinaciones de simbolos + y menos 
+        # dependiendo de la cantidad de nodos
+        simC = itertools.product(*[['+', '-'] for sim in sim_s])
+        combis = [''.join(el) for el in simC]
+
+        # Generando las llaves de las posibles combinaciones de simbolos
+        # y añadiendolas al diccionario
+        keys = []
+        for i in range(len(combis)):
+            key = ""
+            for j in range(len(combis[i])):
+                key += combis[i][j] + sim_s[j] 
+            keys.append(key)        
+        return keys
+
+
+    def nodeFactors(self, node):
+        nodee = [obj for obj in self.graph if obj.value == node][0]
+        keys = self.genKeys(node)
+
+        # Calculando el complemento de las probabilidades
+        probs = nodee.probabilities
+        allP = []
+        for p in probs:
+            allP.append(p)
+            allP.append(1-p)
+        
+        keyC = {}
+        if len(allP) != len(keys):
+            raise ValueError("Hay un error")
+        else:
+            for i, k in enumerate(keys):
+                keyC[k] = allP[i]
+        return keyC
+
+
+    def allFactors(self):
+        factors = {}
+        for node in self.graph:
+            factors[node.value] = self.nodeFactors(node.value)
+        return factors
+    
+
 graph = Graph()
-graph.addEdge("A", "C")
-graph.addEdge("B", "C")
-graph.addEdge("C", "D")
-graph.addEdge("C", "E")
-graph.addEdge("D", None)
-graph.addEdge("E", None)
-# print(graph.checkConections())
-# graph.addProb("E", [0.2, 0.5])
-print(graph.compactness())
+graph.addEdge("R", "A")
+graph.addEdge("T", "A")
+graph.addEdge("A", "J")
+graph.addEdge("A", "M")
+graph.addEdge("J", None)
+graph.addEdge("M", None)
+
+graph.addProb("R", [0.001])
+graph.addProb("T", [0.002])
+graph.addProb("A", [0.95, 0.94, 0.29, 0.001])
+graph.addProb("J", [0.9, 0.05])
+graph.addProb("M", [0.7, 0.01])
+
+print(graph.allFactors())
 
 # for j in graph.graph:
 #     print("val: ", j.value, "probs: ", j.probabilities)
 
-for j in graph.graph:
-    print("val: ", j.value, "dep: ", j.dependencies, "parent: ", j.parent)
+# for j in graph.graph:
+#     print("val: ", j.value, "dep: ", j.dependencies, "parent: ", j.parent)
 
